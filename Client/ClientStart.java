40027b46f1fe0701e0aa148fce922bacd8abcd50
DeathSeeker512 <deathseeker152@gmail.com>
2013-06-16 13:35:15 -0500

    Just added the start files

---
+++ Client/ClientStart.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author deathseeker512
+ * 
+ * @version 0.0.0.0
+ * 
+ * Description: This is a chat client that aims to have a large amount of customizing
+ * options for the user, from the color of the frame to the color of the text. As well
+ * as having several presets that the user can use.
+ */
+
+public class ClientStart {
+    public static void main(String[] args) {
+    }
+}
---
+++ Server/ServerStart.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author deathseeker512
+ * @version 0.0.0.0
+ * Description: This is the server that will send and receive packets to all of the
+ * clients that are connected to it.
+ */
+public class ServerStart {
+    public static void main(String[] args){
+        
+    }
+}
